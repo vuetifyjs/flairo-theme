21570f79ffc184c1da962401035c2d31a391696f
wb-ts <wb.ts416@gmail.com>
2024-03-14 20:22:20 +0000

    add:overlay feature on project hovering

--- src/components/base/Project.vue
+++ src/components/base/Project.vue
@@ -29,43 +29,35 @@
         cover
         :src="src"
       >
-        <v-fade-transition>
-          <div
-            v-if="isHovering"
-            class="d-flex transition-fast-in-fast-out v-card--reveal primary"
-            style="height: 100%; opacity: .9"
+        <v-overlay
+            :model-value="isHovering"
+            class="d-flex transition-fast-in-fast-out v-card--reveal align-center justify-center text-offblock text-center bg-primary"
+            style="opacity: .8;"
+            contained
           >
-            <v-row
-              class="flex-column"
-              justify="center"
-              align="center"
+            <v-btn
+              :to="`/project/${id}`"
+              variant="tonal"
+              class="ml-n2 mb-2"
+              rounded
+              size="50"
             >
-              <v-btn
-                :to="`/project/${id}`"
-                class="mx-2 mb-2"
-                variant="text"
-                rounded
-                theme="dark"
-                size="x-large"
-              >
-                <v-icon
-                  class="mx-0"
-                  size="x-large"
-                  icon="mdi-link"
-                  color="primary"
-                />
-              </v-btn>
-              <div
-                class="font-weight-black mb-2 text-white"
-                v-text="title"
+              <v-icon
+                class="mx-0"
+                size="40"
+                icon="mdi-link"
+                color="primary"
               />
-              <div
-                class="mb-2 text-white"
-                v-text="subtitle"
-              />
-            </v-row>
-          </div>
-        </v-fade-transition>
+            </v-btn>
+            <div
+              class="font-weight-black mb-2"
+              v-text="title"
+            />
+            <div
+              class="mb-2"
+              v-text="subtitle"
+            />
+        </v-overlay>
       </v-img>
     </v-card>
   </v-hover>
